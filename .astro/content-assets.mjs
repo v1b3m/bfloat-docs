b6159b5e74e4df4785ad4329da1c6d0c3fdeeef3
v1b3m <vibenjamin6@gmail.com>
2025-08-11 10:20:58 +0300

    Add production build

--- .astro/content-assets.mjs
+++
@@ -1,4 +0,0 @@
-
-import __ASTRO_IMAGE_IMPORT_Z1sHVEt from "../../../public/assets/logo.svg?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx";
-export default new Map([["../../../public/assets/logo.svg?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx", __ASTRO_IMAGE_IMPORT_Z1sHVEt]]);
-		
